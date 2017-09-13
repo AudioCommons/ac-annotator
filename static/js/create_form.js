384c4fb074a01903ff0f0c40b7cc0e1cc598a9f0
xavierfav <xavier.favory@gmail.com>
2017-09-13 19:45:23 +0200

    Add step select content type

--- static/js/create_form.js
+++ static/js/create_form.js
@@ -1,8 +1,8 @@
-function create_choice_form(content_type, propriety_name, choices) {
+function create_choice_form(label_name, propriety_name, choices) {
     var form = document.createElement("form");
     form.setAttribute('id', propriety_name);
     var label = document.createElement("label");
-    label.innerHTML = propriety_name;
+    label.innerHTML = label_name;
     form.append(label);
     var break_line = document.createElement("br");
     form.append(break_line);
@@ -22,14 +22,10 @@ function create_choice_form(content_type, propriety_name, choices) {
     return form;
 }
 
-function create_form(schema) {
+function create_final_form(schema, content_type) {
     var global_form = document.createElement("form");
-    global_form.setAttribute('method',"post");
-    global_form.setAttribute('action',"");
-
     var content_types = Object.keys(schema.content_types);
 
-    var content_type = content_types[1];
     var proprieties = schema.content_types[content_type];
     var propriety = "";
     var choices = [];
@@ -38,20 +34,29 @@ function create_form(schema) {
     for (var i = 0, length = proprieties.length; i < length; i++) {
         propriety = proprieties[i];
         choices = schema.proprieties[schema.content_types[content_type][i]];
-        form = create_choice_form(content_type, propriety, choices);
+        form = create_choice_form(propriety, propriety, choices);
         global_form.appendChild(form)
     }
-
     return global_form;
 }
 
-function collectAll(){
+function collect_all(){
     var result = $('form').serializeArray();
     // ADD AJAX POST HERE TO SEND RESULTS TO SERVER
     console.log(result);
 }
 
+function select_content_type(){
+    content_type = $('input[name="content_type"]:checked').val();
+    form = create_final_form(schema, content_type);
+    $("#content_type").hide();
+    $("#content_type_button").hide();
+    $("#form_container").append(form);
+    $("#submit_button").show();
+}
+
 $(document).ready(function() {
-    form = create_form(schema);
+    var content_types = Object.keys(schema.content_types);
+    form = create_choice_form('Content type', 'content_type', content_types);
     $("#form_container").append(form);
 });
